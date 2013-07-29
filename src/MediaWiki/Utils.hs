0a8d384d08094e258fa052cdf0c1f797bc0174b7
Jaime Marquínez Ferrándiz <jaime.marquinez.ferrandiz@gmail.com>
2013-07-29 15:20:24 +0200

    Set a custom User agent in the GET requests

--- src/MediaWiki/Utils.hs
+++ src/MediaWiki/Utils.hs
@@ -5,20 +5,24 @@ module MediaWiki.Utils
 ) where
 
 import Network.HTTP (simpleHTTP, getRequest, getResponseBody)
-import Network.HTTP.Base (defaultGETRequest_)
+import Network.HTTP.Base (defaultGETRequest_, Request)
+import Network.HTTP.Headers (HeaderName(..), findHeader, replaceHeader)
 import Network.URI (parseAbsoluteURI)
 import qualified Data.ByteString as B
 
 type Url = String
 
+customGETRequest url = replaceHeader HdrUserAgent custom_user_agent default_request
+    where uri = case parseAbsoluteURI url of
+            Just uri' -> uri'
+            Nothing -> error ("Wrong url " ++ show url)
+          default_request = defaultGETRequest_ uri
+          Just default_user_agent = findHeader HdrUserAgent default_request
+          custom_user_agent = "mediawiki-dl/0.0 (https://github.com/jaimeMF/mediawiki-dl) " ++ default_user_agent 
+
 getPageContent :: Url -> IO String
-getPageContent url = simpleHTTP (getRequest url) >>= getResponseBody
+getPageContent url = simpleHTTP (customGETRequest url) >>= getResponseBody
 
 getBinaryContent :: Url -> IO B.ByteString 
 getBinaryContent url = do
-    let uri = case parseAbsoluteURI url of
-            Just uri' -> uri'
-            Nothing -> error ("Wrong url " ++ show url)
-    simpleHTTP (defaultGETRequest_ uri) >>= getResponseBody
-
-
+    simpleHTTP (customGETRequest url) >>= getResponseBody
