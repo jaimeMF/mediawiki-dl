5171c2eef1dc53b408c027bc48f10639d9a0a865
Jaime Marquínez Ferrándiz <jaime.marquinez.ferrandiz@gmail.com>
2013-08-01 18:56:19 +0200

    Use System.Console.GetOpt for parsing the arguments

--- mediawiki-dl.hs
+++ mediawiki-dl.hs
@@ -1,18 +1,66 @@
 module Main where
 
 import System.Environment
+import System.Exit
+import System.Console.GetOpt
+
+import Control.Monad
 
 import MediaWiki (downloadArticleReference, articleReferenceFromString, OutputFileFormat (..), DownloadOptions(..), def)
 
+data Opt = Opt
+    { optOutputFormat :: String
+    }
+    deriving (Show)
+
+defaultOpts = Opt
+    { optOutputFormat = "md"
+    }
+
+usageHeader = "Usage: mediawiki-dl [OPTION...] article"
+
+printUsageInfo = putStrLn $ usageInfo usageHeader options
+
+options :: [OptDescr (Opt -> IO Opt)]
+options =
+    [ Option "f" ["format"]
+             (ReqArg
+                (\arg opt -> return opt {optOutputFormat = arg})
+                "FORMAT"
+             )
+             "output format"
+      
+    , Option "h" ["help"]
+             (NoArg
+                (\_ -> do
+                    printUsageInfo
+                    exitSuccess
+                )
+            )
+            "help"
+    ]
+
 main = do
-    article_string:rest <- getArgs
-    let writer = case rest of
-            [] -> Markdown
-            [writer_string] -> case writer_string of
+    (actions, args, errors) <- fmap (getOpt Permute options) getArgs
+
+    opts <- foldl (>>=) (return defaultOpts) actions
+    let Opt {optOutputFormat = outputFormat} = opts
+
+    unless (null errors) $ do
+        putStrLn "Error on arguments parsing:"
+        putStrLn $ concat errors
+        exitFailure
+
+    when (null args) $ do
+        putStrLn usageHeader
+        putStrLn "no article given"
+        exitFailure
+
+    let writer = case outputFormat of
                 "md" -> Markdown
                 "html" -> HTML
                 "epub" -> EPUB
                 _ -> error "Unsoported output format"
-        options = def {outputFormat = writer}
-    downloadArticleReference (articleReferenceFromString article_string) options
+        dlOptions = def {outputFormat = writer}
+    downloadArticleReference (articleReferenceFromString $ head args) dlOptions
     return ()
